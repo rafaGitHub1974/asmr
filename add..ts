2f822aee23dd07ea9c41ed116a3fbe1edcc14848
rafa <esrafaelaliende@gmail.com>
2024-03-03 17:23:32 +0100

    initial com

--- add..ts
+++ add..ts
@@ -2,6 +2,6 @@ function addNumbers(a: number, b: number) {
     return a + b; 
 } 
 
-var sum: number = addNumbers(10, 15) 
+var sum = addNumbers(10, 15) 
 
 console.log('Sum of the two numbers is: ' +sum); 
